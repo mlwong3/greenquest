54446cfb43a856fff0c7bb59f8aca08c0d7ee2b9
mlwong3 <mlwong3@stfa-yyc.edu.hk>
2026-01-26 16:07:27 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-    API_KEY: "AIzaSyDigbJEdIP9mWFslWPtA2piRI4josfLPbE"
+    API_KEY: "5945fd7211074502bd9147fdbd3dda3f.6iMkndttoFjFfSoN",  // Get from https://z.ai/manage-apikey/apikey-list
+    API_PROVIDER: "zai"  // Using Z.ai GLM API
 };
