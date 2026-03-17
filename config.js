fd6d2f93e3eb1dc7eac12a1e1b439d6e789cd66b
mlwong3 <mlwong3@stfa-yyc.edu.hk>
2026-03-17 23:32:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_KEY: "5945fd7211074502bd9147fdbd3dda3f.6iMkndttoFjFfSoN",  // Get from https://z.ai/manage-apikey/apikey-list
+    API_KEY: "
     API_PROVIDER: "zai"  // Using Z.ai GLM API
 };
